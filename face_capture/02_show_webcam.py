ef975218139279bff003338dec084cd74d92d866
RyuKoki <68011889+RyuKoki@users.noreply.github.com>
2022-08-17 11:54:37 +0700

    Update 02_show_webcam.py

--- face_capture/02_show_webcam.py
+++ face_capture/02_show_webcam.py
@@ -11,4 +11,21 @@ webcam_index = 0
 # webcam object
 capture = cv2.VideoCapture( webcam_index )
 # set width and height for openCV
-
+capture.set( cv2.CAP_PROP_FRAME_WIDTH, image_width )
+capture.set( cv2.CAP_PROP_FRAME_HEIGHT, image_height )
+# super loop for video capture and display on window
+while (True):
+    # get video capture
+    ret, frame = capture.read()
+    # display the video capture
+    cv2.imshow( "Camera", frame )
+    # waiting for user press input from keyboard
+    command = cv2.waitKey(10)
+    # if press 'q' from keyboard is to quit
+    if command == ord('q'):
+        print("Ending the program")
+        break
+    # if press 's' from keyboard is to save the picture
+    elif command == ord('s'):
+        print("Saving the image")
+        cv2.imwrite( "test.jpg", frame )
